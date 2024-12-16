032cb9b488e90fe7a9862fb0634a4e35e92ad6af
Egor <egorka.tan@mail.ru>
2024-12-16 22:21:49 +0300

    Create movies database, add database class and config

---
+++ site.local/config/database.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'driver' => 'mysql',
+    'host' => 'MySQL-8.2',
+    'port' => 3306,
+    'database' => 'movies-db',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8',
+];
---
+++ site.local/kernel/Database/Database.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Kernel\Database;
+
+use App\Kernel\Config\ConfigInterface;
+
+class Database implements DatabaseInterface
+{
+    private \PDO $pdo;
+
+    public function __construct(
+        private ConfigInterface $config
+    ) {
+        $this->connect();
+    }
+
+    public function insert(string $table, array $data): void {}
+
+    private function connect()
+    {
+        $driver = $this->config->get('database.driver');
+        $host = $this->config->get('database.host');
+        $port = $this->config->get('database.port');
+        $database = $this->config->get('database.database');
+        $username = $this->config->get('database.username');
+        $password = $this->config->get('database.password');
+        $charset = $this->config->get('database.charset');
+
+        $this->pdo = new \PDO(
+            "$driver:host=$host;port=$port;dbname=$database;charset=$charset",
+            $username,
+            $password
+        );
+    }
+}
